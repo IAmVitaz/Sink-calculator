66e554305e46894f4f10e8b32de097ab952e63a8
vit_az <vit.azarov@gmail.com>
2020-09-23 08:12:34 -0300

    added activityMagus, added 3 fragments for every action. started to design fragment_main_magus

---
+++ app/src/main/java/com/vitaz/sinkcalculator/MagusFragments/Main/MainMagusFragment.kt
@@ -0,0 +1,20 @@
+package com.vitaz.sinkcalculator.MagusFragments.Main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vitaz.sinkcalculator.R
+
+class MainMagusFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_magus, container, false)
+    }
+
+}
--- app/src/main/java/com/vitaz/sinkcalculator/MainActivity.kt
+++ app/src/main/java/com/vitaz/sinkcalculator/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.vitaz.sinkcalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.vitaz.sinkcalculator.Services.RunesService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        newJob.setOnClickListener {
+            val startNewJob = Intent(this, MagusActivity::class.java)
+            startActivity(startNewJob)
+        }
+
         Log.d("StatList:", RunesService.statList.toString())
 
+
     }
+
+
 
 
 }
