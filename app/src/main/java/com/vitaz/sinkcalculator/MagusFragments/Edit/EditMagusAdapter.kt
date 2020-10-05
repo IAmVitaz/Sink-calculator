35639b2911fd3c03d29ef3b7f23be64eedfa3c26
vit_az <vit.azarov@gmail.com>
2020-10-04 21:15:19 -0300

    modified editMagusAparter to be able to modify parameter in SinkModifier according to info entered. -/+ buttons and switcher are binded as well. modified fraggment_edit_magus layout to get rid of target sink - feature does not make sense

--- app/src/main/java/com/vitaz/sinkcalculator/MagusFragments/Edit/EditMagusAdapter.kt
+++ app/src/main/java/com/vitaz/sinkcalculator/MagusFragments/Edit/EditMagusAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import com.vitaz.sinkcalculator.MagusFragments.Main.MainRuneListAdapter
 import com.vitaz.sinkcalculator.Model.Rune
@@ -15,7 +17,7 @@ import kotlinx.android.synthetic.main.fragment_main_magus_list_item.view.*
 
 class EditMagusAdapter (
     val context: Context,
-    val statList: List<SinkModifier>
+    val statList: MutableList<SinkModifier>
 ): RecyclerView.Adapter<EditMagusAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -33,10 +35,51 @@ class EditMagusAdapter (
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        // Arrange Negative Sink block
+        holder.itemView.negativeSinkLayout.visibility = View.GONE
+        holder.itemView.negativeLayoutSwitch.setOnCheckedChangeListener { _, switchIsChecked ->
+            if (switchIsChecked) {
+                holder.itemView.negativeSinkLayout.visibility = View.VISIBLE
+                holder.itemView.statNameLayout.visibility = View.GONE
+            } else {
+                holder.itemView.negativeSinkLayout.visibility = View.GONE
+                holder.itemView.statNameLayout.visibility = View.VISIBLE
+            }
+        }
+
+        holder.itemView.positiveSinkStatMinus.setOnClickListener {
+            statList[position].statPositive--
+            holder.itemView.positiveSinkStat.setText(statList[position].statPositive.toString())
+        }
+
+        holder.itemView.positiveSinkStatPlus.setOnClickListener {
+            statList[position].statPositive++
+            holder.itemView.positiveSinkStat.setText(statList[position].statPositive.toString())
+        }
+
+        holder.itemView.negativeSinkStatMinus.setOnClickListener {
+            statList[position].statNegative--
+            holder.itemView.negativeSinkStat.setText(statList[position].statNegative.toString())
+        }
+
+        holder.itemView.negativeSinkStatPlus.setOnClickListener {
+            statList[position].statNegative++
+            holder.itemView.negativeSinkStat.setText(statList[position].statNegative.toString())
+        }
+
+        holder.itemView.positiveSinkStat.addTextChangedListener {
+            statList[position].statPositive = it.toString().toInt()
+        }
+
+        holder.itemView.negativeSinkStat.addTextChangedListener {
+            statList[position].statNegative = it.toString().toInt()
+        }
+
         holder.itemView.statName.text = statList[position].statName
 
         val resourceId = context.resources.getIdentifier(statList[position].statImage, "drawable", context.packageName)
         holder.itemView.statImage.setImageResource(resourceId)
+
 
     }
 }
